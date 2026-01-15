143a06914b7555b547533543402445a2491a35e2
Alfredo Salzillo <noctisbels@gmail.com>
2026-01-15 16:13:37 +0100

    feat: update deps and code (#85)
    * chore(deps): updated dependencies
    * chore(linting): migrated to biome
    * chore(pipeline): updated pipeline dependencies

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,9 @@
-import fastGlob from 'fast-glob';
-import path from 'path';
-import type { Plugin } from 'rollup';
-import type FastGlob from 'fast-glob';
+import path from "node:path";
+import type FastGlob from "fast-glob";
+import fastGlob from "fast-glob";
+import type { Plugin } from "rollup";
 
-const pluginName = 'rollup-plugin-multi-input';
+const pluginName = "rollup-plugin-multi-input";
 
 const doYouNeedRollupMessage = `
   \u001b[31mATTENTION:\u001b[0m Do you still need \u001b[34m${pluginName}\u001b[0m?
@@ -11,26 +11,25 @@ const doYouNeedRollupMessage = `
   and \u001b[32m[preserveModulesRoot](https://rollupjs.org/configuration-options/#output-preservemodulesroot)\u001b[0m options are available,
   which can often eliminate the necessity of \u001b[34m${pluginName}\u001b[0m.
 `;
-const isString = (value: any): value is string => typeof value === 'string';
+const isString = (value: unknown): value is string => typeof value === "string";
 
 /**
  * default multi-input Options
  * */
 const defaultOptions = {
-  // `path.sep` is used for windows support
-  relative: `src${path.sep}`,
+	// `path.sep` is used for windows support
+	relative: `src${path.sep}`,
 };
 
 // extract the output file name from a file name
-const outputFileName = (filePath: string) => filePath
-  .replace(/\.[^/.]+$/, '')
-  .replace(/\\/g, '/');
+const outputFileName = (filePath: string) =>
+	filePath.replace(/\.[^/.]+$/, "").replace(/\\/g, "/");
 
 export type MultiInputOptions = {
-  glob?: FastGlob.Options
-  relative?: string
-  transformOutputPath?: (path: string, fileName: string) => string
-  hideBuildStartMessage?: boolean
+	glob?: FastGlob.Options;
+	relative?: string;
+	transformOutputPath?: (path: string, fileName: string) => string;
+	hideBuildStartMessage?: boolean;
 };
 
 /**
@@ -38,53 +37,55 @@ export type MultiInputOptions = {
  *  structure in the dist folder
  * */
 const multiInput = (options: MultiInputOptions = defaultOptions): Plugin => {
-  const {
-    glob: globOptions,
-    relative = defaultOptions.relative,
-    transformOutputPath,
-    hideBuildStartMessage,
-  } = options;
-  return ({
-    name: pluginName,
-    buildStart() {
-      if (this.info && hideBuildStartMessage !== false) {
-        this.info(doYouNeedRollupMessage);
-      }
-    },
-    options(conf) {
-      // flat to enable input to be a string or an array
-      const inputs = [conf.input].flat();
-      // separate globs inputs string from others to enable input to be a mixed array too
-      const globs = inputs.filter(isString);
-      const others = inputs.filter((value) => !isString(value));
-      const normalizedGlobs = globs.map((glob) => glob.replace(/\\/g, '/'));
-      // get files from the globs strings and return as a Rollup entries Object
-      const entries = fastGlob
-        .sync(normalizedGlobs, globOptions)
-        .map((name) => {
-          const filePath = path.relative(relative, name);
-          const isRelative = !filePath.startsWith(`..${path.sep}`);
-          const relativeFilePath = (isRelative
-            ? filePath
-            : path.relative(`.${path.sep}`, name));
-          if (transformOutputPath) {
-            return [outputFileName(transformOutputPath(relativeFilePath, name)), name];
-          }
-          return [outputFileName(relativeFilePath), name];
-        });
-      const input = Object
-        .assign(
-          {},
-          Object.fromEntries(entries),
-          // add no globs input to the result
-          ...others,
-        );
-      // return the new configuration with the glob input and the non string inputs
-      return {
-        ...conf,
-        input,
-      };
-    },
-  });
+	const {
+		glob: globOptions,
+		relative = defaultOptions.relative,
+		transformOutputPath,
+		hideBuildStartMessage,
+	} = options;
+	return {
+		name: pluginName,
+		buildStart() {
+			if (this.info && hideBuildStartMessage !== false) {
+				this.info(doYouNeedRollupMessage);
+			}
+		},
+		options(conf) {
+			// flat to enable input to be a string or an array
+			const inputs = [conf.input].flat();
+			// separate globs inputs string from others to enable input to be a mixed array too
+			const globs = inputs.filter(isString);
+			const others = inputs.filter((value) => !isString(value));
+			const normalizedGlobs = globs.map((glob) => glob.replace(/\\/g, "/"));
+			// get files from the globs strings and return as a Rollup entries Object
+			const entries = fastGlob
+				.sync(normalizedGlobs, globOptions)
+				.map((name) => {
+					const filePath = path.relative(relative, name);
+					const isRelative = !filePath.startsWith(`..${path.sep}`);
+					const relativeFilePath = isRelative
+						? filePath
+						: path.relative(`.${path.sep}`, name);
+					if (transformOutputPath) {
+						return [
+							outputFileName(transformOutputPath(relativeFilePath, name)),
+							name,
+						];
+					}
+					return [outputFileName(relativeFilePath), name];
+				});
+			const input = Object.assign(
+				{},
+				Object.fromEntries(entries),
+				// add no globs input to the result
+				...others,
+			);
+			// return the new configuration with the glob input and the non string inputs
+			return {
+				...conf,
+				input,
+			};
+		},
+	};
 };
 export default multiInput;
